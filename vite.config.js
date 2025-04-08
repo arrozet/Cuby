2baba8221569965f47befc3dd1bc51f093e18422
RubénO. <rubenoliva@uma.es>
2025-04-08 13:37:01 +0200

    Update vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Add base path for GitHub Pages - use your repository name
+  base: '/Cuby/',
+})
